53c1fc4b76803d7af645988dd6d31e998c63236a
sandeep_insa <sandeep.insa32@gmail.com>
2021-03-25 22:12:08 +0530

    feat:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,221 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	AppBar,
+	Button,
+	Card,
+	CardActions,
+	CardMedia,
+	CssBaseline,
+	Grid,
+	Toolbar,
+	CardContent,
+	Typography,
+	Container,
+	Link,
+} from '@material-ui/core';
+import useStyles from './App_Style';
+
+function Matfooter() {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	);
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles();
+
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<AppBar
+				position="sticky"
+				color="transparent"
+				className={classes.navbarMain}
+				maxWidth="md">
+				<Toolbar>
+					<Grid
+						justify="space-between" // Add it here :)
+						container
+						spacing={24}>
+						<Grid item>
+							<img
+								src="https://whatpageareyouon.com/scenesof/wp-content/uploads/2020/07/logo.png"
+								alt="image_logo"
+								className={classes.navbarLogo}
+							/>
+						</Grid>
+
+						<Grid item>
+							<Button
+								variant="contained"
+								size="small"
+								color="primary"
+								align="right"
+								className={classes.btnJoin}>
+								Join
+							</Button>
+
+							<Button
+								color="transparent"
+								variant="outlined"
+								size="small"
+								align="right"
+								className={classes.btnSign}>
+								Sign
+							</Button>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+
+			<main className={classes.mai}>
+				<div className={classes.herContent}>
+					<div maxWidth="md" className={classes.main}>
+						<Typography
+							component="h2"
+							variant="h4"
+							align="center"
+							color="textPrimary"
+							gutterBottom>
+							Capture, Share, Explore.
+						</Typography>
+
+						<Typography
+							variant="h6"
+							align="center"
+							color="textSecondary"
+							paragraph>
+							A world of great photo contests and awards, in one
+							place.
+						</Typography>
+
+						<div className={classes.btnExplore}>
+							<Grid container spacing={2} justify="center">
+								<Grid item>
+									<Button variant="outlined" color="primary">
+										Explore the World!
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</div>
+				</div>
+
+				<div className={classes.cardGrid} maxWidth="lg">
+					<Grid container spacing={2} justify="center">
+						<Grid item>
+							<Typography
+								gutterBottom
+								variant="h4"
+								component="h4">
+								Enter a contest for free now
+							</Typography>
+						</Grid>
+					</Grid>
+
+					{/* End hero unit */}
+					<Grid container spacing={2}>
+						{cards.map((card) => (
+							<Grid item key={card} xs={12} sm={6} md={3}>
+								<Card className={classes.card}>
+									<CardMedia
+										className={classes.cardMedia}
+										image="https://source.unsplash.com/random"
+										title="Image title"
+									/>
+									<CardContent
+										className={classes.cardContent}>
+										<Grid
+											container
+											spacing={2}
+											justify="center">
+											<Grid item>
+												<Typography
+													gutterBottom
+													variant="h6"
+													component="h6">
+													Traveling
+												</Typography>
+											</Grid>
+										</Grid>
+										<Grid
+											container
+											spacing={2}
+											justify="flex-end">
+											<Grid item>
+												<Typography
+													gutterBottom
+													variant="p"
+													component="p"
+													color="textSecondary">
+													-Brand Name
+												</Typography>
+											</Grid>
+										</Grid>
+									</CardContent>
+									<CardActions>
+										<Grid
+											container
+											spacing={2}
+											justify="center">
+											<Grid item>
+												<Button
+													size="small"
+													variant="outlined"
+													color="primary"
+													className={
+														classes.btnJoinCon
+													}>
+													Join Contest
+												</Button>
+											</Grid>
+										</Grid>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
+					<div className={classes.btnExplore}>
+						<Grid container spacing={2} justify="center">
+							<Grid item>
+								<Button
+									variant="contained"
+									color="default"
+									size="small"
+									className={classes.btnMoreCon}>
+									`See more open contests${ >} `
+								</Button>
+							</Grid>
+						</Grid>
+					</div>
+				</div>
+			</main>
+			{/* Footer */}
+			<footer className={(classes.footer, classes.main)}>
+				<Typography variant="h6" align="center" gutterBottom>
+					Footer
+				</Typography>
+				<Typography
+					variant="subtitle1"
+					align="center"
+					color="textSecondary"
+					component="p">
+					Something here to give the footer a purpose!
+				</Typography>
+				<Matfooter />
+			</footer>
+			{/* End footer */}
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/App_Style.js
@@ -0,0 +1,85 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const imgSrc =
+	'https://images.unsplash.com/photo-1548195667-1d329af0a472?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1782&q=80';
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& body': {},
+	},
+	icon: {
+		marginRight: theme.spacing(2),
+	},
+	main: {
+		minHeight: '100vh',
+		padding: '100px 0',
+		background: `url('${imgSrc}') no-repeat  fixed red`,
+		overflow: 'none',
+
+		// https://scenesofnewengland.com/wp-content/uploads/2020/07/landscape-4518381_1920.png
+	},
+	heroContent: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(8, 0, 6),
+	},
+	navbarMain: {
+		backgroundColor: '#1e1e1e',
+		// backgroundColor: 'steelblue',
+	},
+	btnExplore: {
+		marginTop: theme.spacing(4),
+	},
+	cardGrid: {
+		paddingTop: theme.spacing(8),
+		paddingBottom: theme.spacing(8),
+	},
+	card: {
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	cardMedia: {
+		paddingTop: '75%', // 16:9 56.25%'
+	},
+	cardContent: {
+		flexGrow: '1',
+	},
+	footer: {
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(6),
+	},
+	navbarLogo: {
+		marginTop: '20px',
+		height: '50px',
+	},
+	btnJoin: {
+		color: '#fff',
+		backgroundColor: '#5AD9E6',
+		margin: '20px 10px',
+		fontSize: '12px',
+		justifyContent: 'center',
+	},
+	btnSign: {
+		color: '#fff',
+		borderColor: '#fff',
+		backgroundColor: 'transparent',
+		margin: '20px 10px',
+		textTransform: 'normal',
+		fontSize: '12px',
+	},
+	btnJoinCon: {
+		paddingLeft: '20px',
+		paddingRight: '20px',
+	},
+	btnMoreCon: {
+		color: '#fff',
+		fontSize: '12px',
+		padding: '10px',
+		backgroundColor: '#3dad4b',
+		borderRadius: '10px',
+		'&:hover': {
+			backgroundColor: '#389e44',
+		},
+	},
+}));
+
+export default useStyles;
