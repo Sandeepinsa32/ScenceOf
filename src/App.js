13223e782af88ef3c83a19de4bb1413f82ad7e12
sandeep_insa <sandeep.insa32@gmail.com>
2021-03-27 10:09:13 +0530

    feat: adding nav icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	AppBar,
 	Button,
@@ -12,12 +12,19 @@ import {
 	Typography,
 	Container,
 	Link,
+	spacing,
+	Menu,
+	MenuItem,
+	IconButton,
+	InputBase,
+	Paper,
+	CardActionArea,
 } from '@material-ui/core';
 import useStyles from './App_Style';
-import KeyboardArrowDownOutlinedIcon from '@material-ui/icons/KeyboardArrowDownOutlined';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Matfooter() {
 	return (
@@ -33,9 +40,17 @@ function Matfooter() {
 }
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const test = [1, 2, 3];
 
 function App() {
 	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
 	return (
 		<React.Fragment>
@@ -81,47 +96,142 @@ function App() {
 				</Toolbar>
 			</AppBar>
 
-			<main className={classes.mai}>
+			<main>
 				<div className={classes.herContent}>
 					<div maxWidth="md" className={classes.main}>
-						<Typography
-							component="h2"
-							variant="h4"
-							align="center"
-							color="textPrimary"
-							gutterBottom>
-							Capture, Share, Explore.
-						</Typography>
+						<nav>
+							<Grid
+								justify="space-evenly" // Add it here :)
+								container
+								spacing={24}>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									Home
+								</Link>
 
-						<Typography
-							variant="h6"
-							align="center"
-							color="textSecondary"
-							paragraph>
-							A world of great photo contests and awards, in one
-							place.
-						</Typography>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									About
+								</Link>
 
-						<div className={classes.btnExplore}>
-							<Grid container spacing={2} justify="center">
-								<Grid item>
-									<Button variant="outlined" color="primary">
-										<a
-											href="#seeThis"
-											className={classes.seeThisLink}>
-											Explore More
-										</a>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									<Button
+										aria-controls="simple-menu"
+										aria-haspopup="true"
+										onClick={handleClick}
+										className={classes.link}>
+										Contests
 									</Button>
+									<Menu
+										id="simple-menu"
+										anchorEl={anchorEl}
+										keepMounted
+										open={Boolean(anchorEl)}
+										onClose={handleClose}>
+										<MenuItem onClick={handleClose}>
+											categories
+										</MenuItem>
+										<MenuItem onClick={handleClose}>
+											Ending Soon
+										</MenuItem>
+										<MenuItem onClick={handleClose}>
+											Popular
+										</MenuItem>
+										<MenuItem onClick={handleClose}>
+											Winner
+										</MenuItem>
+									</Menu>
+								</Link>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									Calender
+								</Link>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									Directory
+								</Link>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									Sponsor
+								</Link>
+								<Link
+									variant="button"
+									color="primary"
+									href="#"
+									className={classes.link}>
+									Blog
+								</Link>
+							</Grid>
+						</nav>
+						<div className={classes.headingDiv}>
+							<Typography
+								component="h3"
+								variant="h3"
+								p={1}
+								align="center"
+								color="textPrimary"
+								className={classes.mainHeading}
+								gutterBottom>
+								Capture, Share, Explore.
+							</Typography>
+
+							<Grid
+								justify="center" // Add it here :)
+								container
+								spacing={24}>
+								<Grid item>
+									<Paper
+										component="form"
+										className={classes.rootD}>
+										<InputBase
+											className={classes.input}
+											placeholder="Search a Content"
+											// inputProps={{
+											// 	'aria-label':
+											// 		'search google maps',
+											// }}
+										/>
+										<IconButton
+											type="submit"
+											className={classes.iconButton}
+											aria-label="search">
+											<SearchIcon />
+										</IconButton>
+									</Paper>
 								</Grid>
 							</Grid>
+							{/* <Typography
+								variant="h6"
+								align="center"
+								color="textSecondary"
+								className={classes.mainHeading}
+								paragraph>
+								A world of great photo contests and awards, in
+								one place.
+							</Typography> */}
 						</div>
 					</div>
 				</div>
-
-				<Container
-					className={classes.cardGrid}
-					id="seeThis"
-					maxWidth="lg">
+				<Container className={classes.cardGrid} maxWidth="lg">
 					<Grid container spacing={2} justify="center">
 						<Grid item>
 							<Typography
@@ -203,30 +313,85 @@ function App() {
 									color="default"
 									size="small"
 									className={classes.btnMoreCon}>
-									See more open contests >
+									See more open contests
 								</Button>
 							</Grid>
 						</Grid>
 					</div>
 				</Container>
+
+				{/* testimonial */}
+				<Container className={classes.cardGrid} maxWidth="lg">
+					<Grid container>
+						{test.map((card) => (
+							<Card
+								className={classes.root}
+								key={card}
+								xs={12}
+								sm={4}
+								md={3}>
+								<CardActionArea>
+									<CardContent>
+										<Grid
+											container
+											spacing={2}
+											justify="center">
+											<Grid item>
+												<CardMedia
+													className={classes.media}
+													image="https://scenesofnewengland.com/wp-content/uploads/2020/07/pexels-photo-220453.png"
+												/>
+											</Grid>
+
+											<Grid item>
+												<Typography
+													variant="body2"
+													color="textSecondary"
+													component="p">
+													I love this site, so many
+													amazing photos that inspired
+													me every single day
+												</Typography>
+												<Typography
+													variant="subtitle1"
+													component="h6"
+													align="right">
+													-John Kei
+												</Typography>
+											</Grid>
+										</Grid>
+									</CardContent>
+								</CardActionArea>
+							</Card>
+						))}
+					</Grid>
+				</Container>
+				{/* testimonial end */}
 			</main>
 			{/* Footer */}
 			<footer className={classes.footer}>
-				<Typography variant="h6" align="center" gutterBottom>
+				<Typography
+					variant="h6"
+					align="center"
+					className={classes.mainHeading}
+					gutterBottom>
 					Follow Us On
 				</Typography>
-				<Grid container spacing={4} justify="center">
+
+				<Grid spacing={2} align="center">
 					<Grid item>
-						<FacebookIcon />
-						<InstagramIcon />
-						<TwitterIcon />
+						<FacebookIcon className={classes.mainHeading} />
+						<InstagramIcon className={classes.mainHeading} />
+						<TwitterIcon className={classes.mainHeading} />
 					</Grid>
 				</Grid>
+
 				<Typography
 					variant="subtitle1"
 					align="center"
 					color="textSecondary"
-					component="p">
+					component="p"
+					className={classes.mainHeading}>
 					Something here to give the footer a purpose!
 				</Typography>
 				<Matfooter />
--- src/App_Style.js
+++ src/App_Style.js
@@ -1,21 +1,57 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const imgSrc =
-	'https://images.unsplash.com/photo-1548195667-1d329af0a472?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1782&q=80';
+	'https://images.unsplash.com/photo-1564475228765-f0c3292f2dec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1956&q=80';
+// 'https://images.unsplash.com/photo-1548195667-1d329af0a472?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1782&q=80';
 const useStyles = makeStyles((theme) => ({
+	rootD: {
+		padding: '2px 4px',
+		display: 'flex',
+		alignItems: 'center',
+		width: 400,
+	},
+	input: {
+		marginLeft: theme.spacing(1),
+		flex: 1,
+	},
+	iconButton: {
+		padding: 10,
+		color: '#FD9B28',
+	},
+	divider: {
+		height: 28,
+		margin: 4,
+	}, // search bar icon
 	root: {
-		'& body': {},
+		maxWidth: '400px',
+		margin: '10px auto',
+		borderRadius: '20px',
+	},
+	mainHeading: {
+		color: '#FFF',
+	},
+	media: {
+		height: '80px',
+		width: '80px',
+		borderRadius: '50%',
+	},
+	headingDiv: {
+		marginTop: '10vh',
 	},
 	icon: {
 		marginRight: theme.spacing(2),
 	},
 	main: {
 		minHeight: '100vh',
-		padding: '100px 0',
+		padding: '25px 0',
 		background: `url('${imgSrc}') no-repeat  fixed red`,
 		overflow: 'none',
-
-		// https://scenesofnewengland.com/wp-content/uploads/2020/07/landscape-4518381_1920.png
+	},
+	link: {
+		color: '#fff',
+		fontSize: '16px',
+		textDecoration: 'auto',
+		textTransform: 'none',
 	},
 	heroContent: {
 		backgroundColor: theme.palette.background.paper,
@@ -83,6 +119,10 @@ const useStyles = makeStyles((theme) => ({
 		borderRadius: '10px',
 		'&:hover': {
 			backgroundColor: '#389e44',
+		},
+		cardName: {
+			fontSize: '12px',
+			color: 'red',
 		},
 	},
 }));
